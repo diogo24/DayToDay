061abee521110565e11e04ff2019b0e91bd0178a
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-06-17 16:22:06 +0100

    version 1 - world cup matches (exception on spliting lines from csv file)

---
+++ DailyManagementTemplate/DailyManagementTemplate/Controllers/WorldCupController.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DailyManagementTemplate.Models;
+using System.IO;
+using System.Text;
+
+namespace DailyManagementTemplate.Controllers
+{
+    public class WorldCupController : Controller
+    {
+
+        public IActionResult Index() {
+            var worldCupModel = new WorldCupViewModel();
+
+            GetWorldCupMatches(worldCupModel);
+            worldCupModel.Groups          = GetWorldCupGroups();
+            worldCupModel.EliminationFase = GetEliminationFaseRounds();
+            worldCupModel.NumbertOfRounds = Enum.GetValues(typeof(EliminationFaseRoundEnum)).Length;
+
+            return View(worldCupModel);
+        }
+
+        private void GetWorldCupMatches(WorldCupViewModel worldCupViewModel)
+        {
+            var worldCupFileData = ReadFile();
+
+            ParseWorldCupData(worldCupFileData, worldCupViewModel);
+
+            //throw new NotImplementedException();
+        }
+
+        private void ParseWorldCupData(string worldCupFileData, WorldCupViewModel worldCupViewModel)
+        {
+            using (var sr = new StringReader(worldCupFileData))
+            {
+                string line;
+
+                // load header
+                line = sr.ReadLine();
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    var split       = line.Split(',');
+                    var day         = split[0];
+                    var date        = split[1];
+                    var match       = split[2];
+                    var matchNumber = int.Parse(split[3]);
+
+                    MatchParse(match, matchNumber, day, date);
+                }
+            }
+        }
+
+        private void MatchParse(string match, int matchNumber, string day, string date)
+        {
+            if (matchNumber <= 48)
+            {
+                var groupMatchViewModel = new GroupMatchViewModel();
+                
+                SetGroup(match, groupMatchViewModel);
+                SetHomeTeam(match, groupMatchViewModel.Match);
+                SetAwayTeam(match, groupMatchViewModel.Match);
+                SetScore(match, groupMatchViewModel.Match);
+
+                groupMatchViewModel.Match.Day  = day;
+                groupMatchViewModel.Match.Date = date;
+            }
+            else {
+
+            }
+        }
+
+        private void SetScore(string match, MatchViewModel matchViewModel)
+        {
+            var startIndex = match.IndexOf('(');
+
+            if (startIndex >= 0)
+            {
+                var score = match.Substring(startIndex + 1).Remove(')').Split('?');
+                matchViewModel.HomeTeamScore = int.Parse(score[0]);
+                matchViewModel.AwayTeamScore = int.Parse(score[1]);
+            }
+        }
+
+        private void SetAwayTeam(string match, MatchViewModel matchViewModel)
+        {
+
+            var startIndex = match.IndexOf('-');
+            var endIndex = match.IndexOf('(');
+
+            if (endIndex < 0)
+            {
+                var awayTeam = match.Substring((startIndex + 1));
+                matchViewModel.AwayTeam = new TeamViewModel { TeamName = awayTeam.Trim() };
+            }
+            else {
+                var awayTeam = match.Substring((startIndex + 1), (endIndex - startIndex - 1));
+                matchViewModel.AwayTeam = new TeamViewModel { TeamName = awayTeam.Trim() };
+            }
+            
+        }
+
+        private void SetHomeTeam(string match, MatchViewModel matchViewModel)
+        {
+            var startIndex = match.IndexOf(']');
+            var endIndex = match.IndexOf('-');
+            var homeTeam = match.Substring((startIndex + 1), (endIndex - startIndex - 1));
+            matchViewModel.HomeTeam = new TeamViewModel { TeamName = homeTeam.Trim() };
+        }
+
+        private void SetGroup(string match, GroupMatchViewModel groupMatchViewModel)
+        {
+            var startIndex = match.IndexOf('[');
+            var groupLetter = match.Substring(startIndex + 1, 1);
+            groupMatchViewModel.GroupLetter = Enum.Parse<GroupLetterEnum>(groupLetter);            
+        }
+
+        private static string ReadFile()
+        {
+            string result = string.Empty;
+
+            using (var streamReader = new StreamReader(@"wwwroot\docs\World Cup 2018 Games List.csv"))
+            {
+                result = streamReader.ReadToEnd();
+            }
+
+            return result;
+        }
+
+        private ICollection<GroupTableViewModel> GetWorldCupGroups()
+        {
+            throw new NotImplementedException();
+        }
+
+        private ICollection<EliminationFaseViewModel> GetEliminationFaseRounds()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DailyManagementTemplate/DailyManagementTemplate/Models/WorldCupViewModel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DailyManagementTemplate.Models
+{
+    public class WorldCupViewModel
+    {
+        public WorldCupViewModel()
+        {
+            GroupMatches    = new HashSet<GroupMatchViewModel>();
+            Groups          = new HashSet<GroupTableViewModel>();
+            EliminationFase = new HashSet<EliminationFaseViewModel>();
+        }
+
+        public ICollection<GroupMatchViewModel> GroupMatches { get; set; }
+
+        public ICollection<GroupTableViewModel> Groups { get; set; }
+
+        public ICollection<EliminationFaseViewModel> EliminationFase { get; set; }
+
+        public int NumbertOfRounds { get; set; }
+    }
+
+    public class GroupMatchViewModel {
+
+        public GroupMatchViewModel()
+        {
+            Match = new MatchViewModel();
+        }
+
+        public MatchViewModel Match { get; set; }
+
+        public GroupLetterEnum GroupLetter { get; set; }
+    }
+
+    public class EliminationFaseViewModel
+    {
+        public MatchViewModel Match { get; set; }
+
+        public EliminationFaseRoundEnum EliminationFaaseRound { get; set; }
+
+        public int RoundIndex { get; set; }
+    }
+
+    public enum EliminationFaseRoundEnum {
+        Round16          = 1,
+        QuarterFinals    = 2,
+        SemiFinals       = 3,
+        ThirdFourthPlace = 4,
+        Final            = 5
+    }
+
+    public class GroupTableViewModel {
+        public ICollection<GroupRowViewModel> GroupRows { get; set; }
+
+        public GroupLetterEnum GroupLetter { get; set; }
+    }
+
+    public enum GroupLetterEnum {
+        A = 1,
+        B = 2,
+        C = 3,
+        D = 4,
+        E = 5,
+        F = 6,
+        G = 7,
+        H = 8
+    }
+
+    public class GroupRowViewModel {
+        public TeamViewModel Team { get; set; }
+        public GroupPositionEnum GroupPosition { get; set; }
+
+        public int GamesPlayed{ get; set; }
+        public int Points { get; set; }
+
+        public int Wins { get; set; }
+        public int Draws { get; set; }
+        public int Defeats { get; set; }
+
+        public int GoalsFor { get; set; }
+        public int GoalsAgainst { get; set; }
+    }
+
+    public enum GroupPositionEnum {
+        First  = 1,
+        Second = 2,
+        Third  = 3,
+        Fourth = 4
+    }
+
+    public class MatchViewModel {
+        public TeamViewModel HomeTeam { get; set; }
+        public int? HomeTeamScore { get; set; }
+
+
+        public TeamViewModel AwayTeam { get; set; }
+        public int? AwayTeamScore { get; set; }
+        public string Day { get; internal set; }
+        public string Date { get; internal set; }
+    }
+
+    public class TeamViewModel {
+        public int TeamID { get; set; }
+        public string TeamName { get; set; }
+    }
+}
